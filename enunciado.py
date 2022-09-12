ca4f9788a1138891ad63f1189d4bdf0646504ea2
luizpantaleao <luizeduardofernandes11@hotmail.com>
2022-09-12 20:15:47 -0300

    Add files via upload

---
+++ enunciado.py
@@ -0,0 +1,13 @@
+"""
+    --- Exercicio 1 ---
+    Crie dois arquivos Python: um deverá ser o módulo, que vai conter uma função qualquer, e o outro deve chamar essa função passando argumentos.
+    - - -
+    . deve ser entregue até o dia 13/09 às 14h
+    . deve ser entregue pelo Git
+    - - -
+
+    Qualquer duvida pode colocar no canal de dúvidas.
+"""
+from funcao import soma
+
+soma(5, 5)
---
+++ funcao.py
@@ -0,0 +1,3 @@
+def soma(numero1: int, numero2: int):
+    numero3 = numero1 + numero2
+    print(f'{numero1} + {numero2} = {numero3}')
