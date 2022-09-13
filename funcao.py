862f9b61c0c9ac44465e8986a64e2f7d56393efd
luizpantaleao <luizeduardofernandes11@hotmail.com>
2022-09-13 08:31:54 -0300

    Update funcao.py

--- funcao.py
+++ funcao.py
@@ -1,3 +1,3 @@
 def soma(numero1: float, numero2: float):
-    numero3 = numero1 + numero2
-    print(f'{numero1} + {numero2} = {numero3}')
+    resultado = numero1 + numero2
+    print(f'{numero1} + {numero2} = {resultado}')
