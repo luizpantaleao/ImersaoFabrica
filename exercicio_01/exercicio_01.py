c7bf43ae701531b8b2c8b8f05b3c6c47bd0b9c3b
luizpantaleao <luizeduardofernandes11@hotmail.com>
2022-09-13 08:56:08 -0300

    Update exercicio_01.py

--- exercicio_01/exercicio_01.py
+++ exercicio_01/exercicio_01.py
@@ -1,13 +1,3 @@
-"""
-    --- Exercicio 1 ---
-    Crie dois arquivos Python: um deverá ser o módulo, que vai conter uma função qualquer, e o outro deve chamar essa função passando argumentos.
-    - - -
-    . deve ser entregue até o dia 13/09 às 14h
-    . deve ser entregue pelo Git
-    - - -
-
-    Qualquer duvida pode colocar no canal de dúvidas.
-"""
 from modulo_01 import multiplicacao
 
 fator1 = float(input('Digite o primeiro fator da multiplicação: '))
