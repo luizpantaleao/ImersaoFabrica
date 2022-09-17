24b46ed59ce57da0b6d880c591882ecadceb14c2
Luiz Eduardo Fernandes <luizeduardofernandes11@hotmail.com>
2022-09-17 19:25:31 -0300

    desafio fabrica

--- exercicio_01/exercicio_01.py
+++
@@ -1,6 +0,0 @@
-from modulo_01 import multiplicacao
-
-fator1 = float(input('Digite o primeiro fator da multiplicação: '))
-fator2 = float(input('Digite o segundo fator da multiplicação: '))
-
-multiplicacao(fator1, fator2)
